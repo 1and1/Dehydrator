87b3273bb5da27593492005bf8de3f83d2fbf9b3
Bastian Eicher <bastian@eicher.net>
2015-08-21 13:34:22 +0200

    Added InheritanceRouteProvider

---
+++ Dehydrator.WebApi/Shared/InheritanceRouteProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Controllers;
+using System.Web.Http.Routing;
+
+namespace Dehydrator.WebApi
+{
+    /// <summary>
+    /// A custom WebAPI route provider that enables inheritance on controller attributes.
+    /// </summary>
+    public class InheritanceRouteProvider : DefaultDirectRouteProvider
+    {
+        protected override IReadOnlyList<IDirectRouteFactory> GetActionRouteFactories(
+            HttpActionDescriptor actionDescriptor)
+        {
+            return actionDescriptor.GetCustomAttributes<IDirectRouteFactory>(inherit: true);
+        }
+    }
+}
