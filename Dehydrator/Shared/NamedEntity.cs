ed227701ffbb3c3678cf465b162f42f74adc0462
Bastian Eicher <bastian@eicher.net>
2015-09-14 13:15:10 +0200

    Implemented NamedEntity.ToString()

--- Dehydrator/Shared/NamedEntity.cs
+++ Dehydrator/Shared/NamedEntity.cs
@@ -6,5 +6,12 @@
     public abstract class NamedEntity : Entity, INamedEntity
     {
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return string.IsNullOrEmpty(Name)
+                ? $"{GetType().Name} {Id}"
+                : $"{GetType().Name} {Name} ({Id})";
+        }
     }
 }
