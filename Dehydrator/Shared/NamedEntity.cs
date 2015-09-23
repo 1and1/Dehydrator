df649b2b644d9db16c8a325335275d425e44a86e
Bastian Eicher <bastian@eicher.net>
2015-09-23 13:05:33 +0200

    Marked NamedEntity.Name as [Required]

--- Dehydrator/Shared/NamedEntity.cs
+++ Dehydrator/Shared/NamedEntity.cs
@@ -1,10 +1,13 @@
-﻿namespace Dehydrator
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dehydrator
 {
     /// <summary>
     /// A base implementation of <see cref="INamedEntity"/>. You can derive from this or implement <see cref="INamedEntity"/> directly.
     /// </summary>
     public abstract class NamedEntity : Entity, INamedEntity
     {
+        [Required]
         public string Name { get; set; }
 
         public override string ToString()
