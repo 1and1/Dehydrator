fdb8ee5d89f4328f200747777fe6ed21b121b375
Bastian Eicher <bastian@eicher.net>
2016-01-28 15:59:15 +0100

    Updated copyright notice

--- AssemblyInfo.Global.cs
+++ AssemblyInfo.Global.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("1&1 Internet SE")]
 [assembly: AssemblyProduct("Dehydrator")]
-[assembly: AssemblyCopyright("Copyright © 2015 1und1 Internet SE")]
+[assembly: AssemblyCopyright("Copyright © 1und1 Internet SE")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("0.10.0.0")]
